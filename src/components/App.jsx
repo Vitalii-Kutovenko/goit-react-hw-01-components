a42a56ae64d652e388ed2b76a1be805af02b61ae
Vitalii Kutovenko <idorikk@gmail.com>
2023-06-07 15:47:09 +0300

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
+import React from 'react';
+import _ from 'lodash';
+import user from './user.json';
+import Profile from './Profile/profile';
+import PropTypes from 'prop-types';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        padding: 15,
+        height: '100%',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#366665',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.object,
 };
